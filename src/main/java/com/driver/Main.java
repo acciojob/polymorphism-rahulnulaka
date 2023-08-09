2dafe15105041fb681980e60c6ef234b55a1633b
rahul <rahulsunny988@gmail.com>
2023-08-09 17:43:05 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,6 +11,14 @@ public class Product {
 	}
 }
 public class Main {
-	Product p=new Product();
-	p.product(10.0,5.0);
+	Product p = new Product();
+
+    int result1 = p.product(5, 3);
+    System.out.println("Product of 5 and 3: " + result1);
+
+    int result2 = p.product(2, 4, 6);
+    System.out.println("Product of 2, 4, and 6: " + result2);
+
+    double result3 = p.product(2.5, 3.5);
+    System.out.println("Product of 2.5 and 3.5: " + result3);
 }
