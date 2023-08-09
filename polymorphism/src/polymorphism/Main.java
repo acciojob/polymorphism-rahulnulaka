73e764cc44ca62e81cf1377308a9cb18ebc2464c
rahul <rahulsunny988@gmail.com>
2023-08-09 17:35:52 +0530

    intial commit

--- polymorphism/src/polymorphism/Main.java
+++
@@ -1,5 +0,0 @@
-package polymorphism;
-public class Main {
-Product p=new Product();
-double a=p.product(10.0,5.0);
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,5 +12,5 @@ public class Product {
 }
 public class Main {
 	Product p=new Product();
-	double a=p.product(10.0,5.0);
+	p.product(10.0,5.0);
 }
