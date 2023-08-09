41def3129e28b49d938762791825efd465ab7d76
rahul <rahulsunny988@gmail.com>
2023-08-09 17:31:37 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
-
+public class Product {
+	public int product(int x, int y) {
+		return x+y;
+	}
+	public int product(int x, int y, int z) {
+		return x+y+z;
+	}
+	public double product(double x, double y) {
+		return x+y;
+	}
+}
 public class Main {
-
+	Product p=new Product();
+	double a=p.product(10.0,5.0);
 }
